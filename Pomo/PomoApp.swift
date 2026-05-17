57302ceeb09096d377439a78e8ea2aff60f290e8
ammarsaber-dev <ammarsaber.dev@icloud.com>
2026-05-17 08:23:12 +0300

    Initial Commit

---
+++ Pomo/PomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoApp.swift
+//  Pomo
+//
+//  Created by Ammar Saber on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct PomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
